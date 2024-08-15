89ef5f532c831f2296ca3cccdf8f4b03a8d29dc2
bigotmax <maxence.bigot@reseau.eseo.fr>
2024-08-15 16:25:11 +0200

    create navigation bar

---
+++ components/Elements/Buttons/HamburgerButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <button class="hamburger-button">
+    <span class="hamburger-line" :class="{'line-open': isHambugerOpen}"></span>
+    <span class="hamburger-line" :class="{'line-open': isHambugerOpen}"></span>
+    <span class="hamburger-line" :class="{'line-open': isHambugerOpen}"></span>
+    <span class="hamburger-line" :class="{'line-open': isHambugerOpen}"></span>
+  </button>
+</template>
+
+
+<script setup>
+const _ = defineProps({
+  isHambugerOpen: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+
+
+<style scoped>
+.hamburger-button {
+  @apply w-10 h-[30px] rotate-0 cursor-pointer transition-all duration-500 relative;
+}
+
+.hamburger-line {
+  @apply w-full h-1 bg-foreground rounded-xl opacity-100 absolute left-0;
+  @apply rotate-0 transition-all duration-300;
+  @apply first:top-0 last:top-[26px];
+}
+
+
+.hamburger-line:nth-child(2), .hamburger-line:nth-child(3) {
+  @apply top-[13px];
+}
+
+
+/*-------- OPEN HAMBURGER --------*/
+.line-open {
+  @apply first:w-0 first:top-[13px] first:left-1/2;
+  @apply last:w-0 last:top-[13px] last:left-1/2
+}
+
+.line-open:nth-child(2) {
+  @apply rotate-45;
+}
+
+.line-open:nth-child(3) {
+  @apply -rotate-45;
+}
+</style>
---
+++ components/Menu/MobileNavigationMenu.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="mobile-navigation-menu" :class="{'mobile-menu-open': isMobileMenuOpen}">
+    <div class="menu-container">
+      <MenuLink href="/rental">Location</MenuLink>
+      <MenuLink href="/cars">Véhicules</MenuLink>
+      <MenuLink href="/stores">Nos points de vente</MenuLink>
+      <MenuLink href="/services">Services</MenuLink>
+    </div>
+
+    <div class="buttons-container">
+      <Button type="ghost" href="/signup">S'inscrire</Button>
+      <Button type="default" href="/contact">Prendre contact</Button>
+    </div>
+  </section>
+</template>
+
+
+<script setup>
+const _ = defineProps({
+  isMobileMenuOpen: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+
+<style scoped>
+.mobile-navigation-menu {
+  @apply w-full h-full max-h-0 px-4  bg-background overflow-hidden;
+  @apply flex flex-col gap-5 fixed top-20 left-0 z-50 transition-all duration-500;
+}
+
+.mobile-menu-open {
+  @apply max-h-[330px] py-6 border-b border-border transition-all duration-500;
+}
+
+
+/*-------- MENU CONTAINER --------*/
+.menu-container {
+  @apply flex flex-col items-center gap-3;
+}
+
+
+/*-------- BUTTONS CONTAINER --------*/
+.buttons-container {
+  @apply flex flex-col gap-0.5;
+}
+</style>
